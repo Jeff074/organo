d2590d7dcf049b7f41ac6e145fbcdc2edf715802
Jefferson dos Santos Alves <Jeff074@outlook.com.br>
2023-05-14 11:20:27 -0300

    Organização de arquivos js

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,11 @@
+import'./CampoTexto.css'
+const CampoTexto = (props) => {
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input placeholder='Digite o seu nome'/>
+        </div>
+    )
+}
+
+export default CampoTexto
